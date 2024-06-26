c91d70145558d3d759e631f24ee434384e4aabc5
Theshikanavod <theshikanavod@gmail.com>
2024-06-26 18:08:27 +0530

    registation page updated

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -84,119 +84,121 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="border p-5 rounded" style={{ width: "500px" }}>
-      <h1 className="text-2xl mb-4">Register</h1>
-      {error && <p className="text-red-500">{error}</p>}
-      {!pendingVerification && (
-        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
-          <div>
-            <label
-              htmlFor="firstName"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
-              Company Name
-            </label>
-            <input
-              type="text"
-              name="firstName"
-              id="firstName"
-              onChange={(e) => setFirstName(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
-              required={true}
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="lastName"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
-              Branch Name
-            </label>
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              onChange={(e) => setLastName(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
-              required={true}
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="username"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
-              User Role
-            </label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              onChange={(e) => setUserName(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
-              required={true}
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
-              Email Address
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
-              placeholder="name@company.com"
-              required={true}
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-              required={true}
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-          >
-            Create an account
-          </button>
-        </form>
-      )}
-      {pendingVerification && (
-        <div>
-          <form className="space-y-4 md:space-y-6">
-            <input
-              value={code}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-              placeholder="Enter Verification Code..."
-              onChange={(e) => setCode(e.target.value)}
-            />
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold text-center">Register</h1>
+        {error && <p className="text-red-500 text-center">{error}</p>}
+        {!pendingVerification && (
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="firstName"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Company Name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                onChange={(e) => setFirstName(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                required={true}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="lastName"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Branch Name
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                onChange={(e) => setLastName(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                required={true}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="username"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                User Role
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                onChange={(e) => setUserName(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                required={true}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                placeholder="name@company.com"
+                required={true}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                required={true}
+              />
+            </div>
             <button
               type="submit"
-              onClick={onPressVerify}
-              className="w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              className="w-full text-white bg-[#393939] hover:bg-gray-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
-              Verify Email
+              Create an account
             </button>
           </form>
-        </div>
-      )}
+        )}
+        {pendingVerification && (
+          <div>
+            <form className="space-y-6">
+              <input
+                value={code}
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                placeholder="Enter Verification Code..."
+                onChange={(e) => setCode(e.target.value)}
+              />
+              <button
+                type="submit"
+                onClick={onPressVerify}
+                className="w-full text-white bg-[#393939] hover:bg-gray-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Verify Email
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
