a886ecd186c345e19dec8d6521c21dc14b2837d7
Theshikanavod <theshikanavod@gmail.com>
2024-06-27 14:47:50 +0530

    Update page.tsx

--- frontend/src/app/sign-in/[[...sign-in]]/page.tsx
+++ frontend/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -10,7 +10,7 @@ const signInPage = () => {
       <div className="mb-4">
         <SignIn />
       </div>
-      <a href="/" className="border border-blue-600 bg-blue-600 hover:bg-blue-700 text-white hover:text-white py-3 px-6 rounded-lg shadow-md mt-6 transition duration-300 ease-in-out">
+      <a href="/" className="border border-gray-700 bg-gray-700 hover:bg-gray-800 text-white hover:text-white py-3 px-6 rounded-lg shadow-md mt-6 transition duration-300 ease-in-out">
         Go to Home
       </a>
     </div>
