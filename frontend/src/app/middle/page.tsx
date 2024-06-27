15ea1da896213692b8a7bfd71a8764a202ad5b13
Theshikanavod <theshikanavod@gmail.com>
2024-06-27 06:51:49 +0530

    role auth

--- frontend/src/app/middle/page.tsx
+++ frontend/src/app/middle/page.tsx
@@ -24,13 +24,8 @@ const Middle: React.FC = () => {
 
   return (
     <div>
-      <h1>Login</h1>
-      <a
-        href="/sign-in"
-        className="border border-blue-600 bg-blue-600 hover:bg-blue-700 text-white hover:text-white py-3 px-6 rounded-lg shadow-md mt-6 transition duration-300 ease-in-out"
-      >
-        Buy now
-      </a>
+      <h1>Loading....</h1>
+     
     </div>
   );
 };
